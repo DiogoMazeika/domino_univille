996bfcb3a17d668244bd0f56181e1749fe9a8d4e
diogo_mazeika <mazeikadiogo@gmail.com>
2024-05-06 13:23:37 -0300

    bot mentality

--- script.js
+++ script.js
@@ -34,6 +34,9 @@ $(document).ready(async function () {
   let jogadorPecas = [];
   let botPecas = [];
   let tabuleiroPecas = [];
+  let pontas = [];
+  let jogadas = 0;
+  let skipRes = () => {};
   const music = new Audio();
 
   /* funções auxiliares */
@@ -42,9 +45,9 @@ $(document).ready(async function () {
   };
 
   const thisPontas = (peca, pontas) =>
-    (pecas[peca] ?? []).filter((p) => pontas.includes(p));
+    pontas.filter((p) => pecas[peca]?.includes(p));
 
-  const addPecasTabuleiro = (peca, { x1, x2, y1, y2 }) => {
+  const addPecasTabuleiro = (peca, { x1, x2, y1, y2 }, inverter) => {
     const [p1, p2] = pecas[peca];
     $('#tabuleiro').append(
       $('<div>', {
@@ -53,31 +56,83 @@ $(document).ready(async function () {
       }).append(
         $('<div>', { id: `peca_${peca}`, class: 'peca-container' }).append(
           $('<div>', { class: 'peca' })
-            .append($('<div>', { class: `peca_metade p1 vl_${p1}` }))
-            .append($('<div>', { class: `peca_metade p2 vl_${p2}` }))
+            .append(
+              $('<div>', { class: `peca_metade p1 vl_${inverter ? p2 : p1}` })
+            )
+            .append(
+              $('<div>', { class: `peca_metade p2 vl_${inverter ? p1 : p2}` })
+            )
         )
       )
     );
   };
 
+  const addPlaceholders = (listPositions, [l0, l1], peca) => {
+    const placeholders = [];
+
+    if (peca.includes(l0)) {
+      placeholders.push({
+        div: $('<div>', {
+          class: 'peca-placeholder',
+          id: 'placeholder-start',
+        }),
+        i: 0,
+      });
+    }
+    if (peca.includes(l1)) {
+      placeholders.push({
+        div: $('<div>', {
+          class: 'peca-placeholder',
+          id: 'placeholder-end',
+        }),
+        i: 1,
+      });
+    }
+
+    placeholders.forEach(({ div, i }) => {
+      const { x1, x2, y1, y2 } = listPositions[i];
+      $('#tabuleiro').append(
+        $('<div>', {
+          class: 'tabuleiro-peca-container tabuleiro-placeholder-container',
+          style: `grid-row: ${y1}/${y2}; grid-column: ${x1}/${x2};`,
+        }).append($('<div>', { class: 'peca-container' }).append(div))
+      );
+    });
+
+    return placeholders;
+  };
+
   const addPecasTela = (id, listPecas, extra = '') => {
-    $(`#${id}`).append(
-      listPecas.map((p) => {
-        const [p1, p2] = pecas[p];
-        return $('<div>', { id: `peca_${p}`, class: 'peca-container' }).append(
-          $('<div>', { class: `peca ${extra}` })
-            .append($('<div>', { class: `peca_metade p1 vl_${p1}` }))
-            .append($('<div>', { class: `peca_metade p2 vl_${p2}` }))
-        );
-      })
-    );
+    const pieces = listPecas.map((p) => {
+      const [p1, p2] = pecas[p];
+      return $('<div>', { id: `peca_${p}`, class: 'peca-container' }).append(
+        $('<div>', { class: `peca ${extra}` })
+          .append($('<div>', { class: `peca_metade p1 vl_${p1}` }))
+          .append($('<div>', { class: `peca_metade p2 vl_${p2}` }))
+      );
+    });
+    $(`#${id}`).append(pieces);
+    return pieces;
   };
 
   const loja = async (id, jogador = '') => {
     const vl = pecasInGame[Math.floor(Math.random() * pecasInGame.length)];
     pecasInGame = pecasInGame.filter((p) => p != vl);
-    addPecasTela(`${id}-pecas`, [vl], jogador);
-    return delay(1000).then(() => vl);
+    const [peca] = addPecasTela(`${id}-pecas`, [vl], jogador);
+    if (pecasInGame.length < 1) $('#store').remove();
+
+    if (id === 'bot') {
+      botPecas.push(vl);
+      return delay(1000).then(() => vl);
+    }
+    jogadorPecas.push(vl);
+    return peca;
+  };
+
+  const jogadasPossiveis = (listPecas, pontas) => {
+    return listPecas.filter(
+      (peca) => pecas[peca].filter((p) => pontas.includes(p)).length > 0
+    );
   };
 
   /* funções para controle de audio */
@@ -122,38 +177,39 @@ $(document).ready(async function () {
         list = list.filter((p) => p != vl);
         vl = list[Math.floor(Math.random() * list.length)];
       }
-      return [vl, list.length < 1];
+      return [
+        vl,
+        list.length < 1,
+        thisPontas(vl, pontas).includes(pontas[0]) ? 'start' : 'end',
+      ];
     },
     1: (l, pontas) => {
-      let list = l;
-      let vl;
-      while (thisPontas(vl, pontas).length < 1 && list.length > 0) {
-        list = list.filter((p) => p != vl);
-        vl = list[Math.floor(Math.random() * list.length)];
-      }
-      return [vl, list.length < 1];
-    },
-    2: (l, pontas) => {
-      let list = l;
-      let vl;
-      while (thisPontas(vl, pontas).length < 1 && list.length > 0) {
-        list = list.filter((p) => p != vl);
-        vl = list[Math.floor(Math.random() * list.length)];
-      }
-      return [vl, list.length < 1];
+      // console.debug([...pecasInGame, ...jogadorPecas]);
+      const jogaveis = l
+        .filter((p) => thisPontas(p, pontas).length > 0)
+        .sort((a, b) => b - a);
+      const carroca = jogaveis.find((p) => pecas[p][0] === pecas[p][1]);
+      console.debug(jogaveis, carroca); //.find((p) => pecas[p][0] === pecas[p][1]));
+
+      return [
+        carroca ?? jogaveis[0],
+        jogaveis.length < 1,
+        thisPontas(carroca ?? jogaveis[0], pontas).includes(pontas[0])
+          ? 'start'
+          : 'end',
+      ];
     },
   };
 
   /* funções para controle do jogo */
   const jogoState = async (t, dificuldade) => {
     let turno = t;
-    let pontas = [];
-    const positions = { x1: 1, x2: 4, y1: 3, y2: 5 };
-    while (
-      jogadorPecas.length > 0 &&
-      botPecas.length > 0
-      // && pecasInGame.length > 0
-    ) {
+    let skippings = 0;
+    const positions = {
+      start: { x1: 50, x2: 53, y1: 3, y2: 5 },
+      end: { x1: 53, x2: 56, y1: 3, y2: 5 },
+    };
+    while (jogadorPecas.length > 0 && botPecas.length > 0 && skippings < 2) {
       $('.list-pecas').removeClass('is-turno');
       $(`#${{ 0: 'jogador', 1: 'bot' }[turno]}-pecas`).addClass('is-turno');
       $(`#img-${{ 1: 'jogador', 0: 'bot' }[turno]}`).removeClass('is-turno');
@@ -161,6 +217,7 @@ $(document).ready(async function () {
       if (turno === 0) $('.peca.jogador').addClass('is-turno');
 
       let peca;
+      let position = 'end';
 
       $('.peca.jogador.is-turno').off('click');
 
@@ -170,7 +227,11 @@ $(document).ready(async function () {
         let comprar = false;
 
         if (pontas.length < 1) vl = Math.max(...botPecas);
-        else [vl, comprar] = botJogadaOpts[dificuldade](botPecas, pontas);
+        else
+          [vl, comprar, position] = botJogadaOpts[dificuldade](
+            botPecas,
+            pontas
+          );
 
         if (comprar) {
           let newPeca;
@@ -181,6 +242,9 @@ $(document).ready(async function () {
             newPeca = await loja('bot');
           }
           if (thisPontas(newPeca, pontas).length > 0) vl = newPeca;
+          position = thisPontas(newPeca, pontas).includes(pontas[0])
+            ? 'start'
+            : 'end';
         }
 
         if (vl !== undefined) {
@@ -189,37 +253,119 @@ $(document).ready(async function () {
           peca = vl;
         }
       } else {
-        await new Promise((res) => {
-          $('.peca.jogador.is-turno').on('click', function () {
-            const [_, vl] = $($(this).parent()).attr('id').split('_');
-            const lado = thisPontas(vl, pontas);
-            if (lado.length < 1) {
-              return;
+        const a = async function (res, div) {
+          console.debug(div);
+          $('.tabuleiro-placeholder-container').remove();
+          const [_, vl] = $($(div).parent()).attr('id').split('_');
+          const lados = thisPontas(vl, pontas);
+          if (lados.length < 1) {
+            return;
+          }
+
+          await new Promise((resP) => {
+            addPlaceholders(
+              [positions.start, positions.end],
+              pontas,
+              pecas[vl]
+            ).forEach(({ div: placeholder }) => {
+              $(placeholder).on('click', function () {
+                [, position] = $(this).attr('id').split('-');
+                $('.tabuleiro-placeholder-container').remove();
+                resP();
+              });
+            });
+          });
+          jogadorPecas = jogadorPecas.filter((p) => p != vl);
+          $(div).parent().remove();
+          peca = vl;
+          res();
+        };
+
+        jogadas = jogadasPossiveis(jogadorPecas, pontas).length;
+        $('#store')
+          .off('click')
+          .on('click', async function () {
+            if ($('#jogador-pecas').hasClass('is-turno') && jogadas < 1) {
+              const newPeca = await loja('jogador', 'jogador');
+              $(newPeca)
+                .find('>div')
+                .on('click', async function () {
+                  const $div = this;
+                  a(skipRes, $div);
+                });
+              console.debug(newPeca);
+              jogadas = jogadasPossiveis(jogadorPecas, pontas).length;
             }
-            jogadorPecas = jogadorPecas.filter((p) => p != vl);
-            $(this).parent().remove();
-            peca = vl;
-            res();
+          });
+
+        $('#skip')
+          .off('click')
+          .on('click', function () {
+            if (
+              $('#jogador-pecas').hasClass('is-turno') &&
+              jogadas < 1 &&
+              pecasInGame.length < 1
+            ) {
+              $('.tabuleiro-placeholder-container').remove();
+              skipRes();
+            }
+          });
+
+        await new Promise(async (res) => {
+          skipRes = res;
+          $('.peca.jogador.is-turno').on('click', async function () {
+            const $div = this;
+            a(res, $div);
           });
         });
+        console.debug('res');
       }
 
+      const ps = position === 'start';
+      let inverter = false;
       if (peca !== undefined) {
         if (pontas.length < 1) {
           pontas = pecas[peca];
         } else {
+          inverter = ps
+            ? pontas[0] !== pecas[peca][1]
+            : pontas[1] !== pecas[peca][0];
+
+          pontas = [
+            ps
+              ? pecas[peca].find((p) => !pontas.includes(p)) ?? pecas[peca][1]
+              : pontas[0],
+            !ps
+              ? pecas[peca].find((p) => !pontas.includes(p)) ?? pecas[peca][0]
+              : pontas[1],
+          ];
         }
 
-        console.debug(peca, pecas[peca], pontas);
+        console.debug(pontas);
 
         tabuleiroPecas.push(peca);
-        addPecasTabuleiro(peca, positions);
+        addPecasTabuleiro(peca, positions[position], inverter);
 
-        positions.x1 = positions.x2;
-        positions.x2 = positions.x2 + 3;
+        if (ps) {
+          positions[position].x2 = positions[position].x1;
+          positions[position].x1 = positions[position].x1 - 3;
+        } else {
+          positions[position].x1 = positions[position].x2;
+          positions[position].x2 = positions[position].x2 + 3;
+        }
+        skippings--;
+      } else {
+        skippings++;
       }
 
       turno = !turno * 1;
+    }
+    if (skippings === 2) {
+      alert('empata');
+    } else if (jogadorPecas.length < 1) {
+      alert('winner winner chicken dinner');
+    } else {
+      alert('wasted');
     }
     console.debug('cabo');
   };
@@ -273,6 +419,10 @@ $(document).ready(async function () {
       'src',
       `https://robohash.org/${Date.now()}?set=set4`
     );
+    const $tabuleiroContainer = $('#tabuleiro-container');
+    const scrollLeftMax =
+      $tabuleiroContainer[0].scrollWidth - $tabuleiroContainer[0].clientWidth;
+    $tabuleiroContainer.scrollLeft(scrollLeftMax / 2);
 
     // jogoState(jogadorMaior >= botMaior ? 0 : 1);
     jogoState(1, dificuldade);
@@ -283,10 +433,10 @@ $(document).ready(async function () {
       $('#start').remove();
       $('#dificuldade_txt').removeClass('hidden');
       $('#main_btns').append(
-        ['Fácil', 'Díficil', 'Impossível'].map((text, i) => {
+        ['Fácil', 'Impossível'].map((text, i) => {
           return $('<div>', {
             text,
-            class: `btn mx-1 ${i === 2 ? 'btn_fear' : ''}`,
+            class: `btn mx-1 ${i === 1 ? 'btn_fear' : ''}`,
           }).on('click', () => start(i));
         })
       );
